c32e72d8233a0f1a3fc30b56226531ac15ef7faf
albertbahia <albert.bahia1@gmail.com>
2015-01-01 21:51:07 -0500

    added httparty gem, edmunds env variable and initial class method

--- app/models/edmunds.rb
+++ app/models/edmunds.rb
@@ -1,6 +1,8 @@
 class Edmunds
 
 	def self.car_search(query)
+		url = URI.escape("https://api.edmunds.com/api/vehicle/v2/makes?state=used&year=2010&view=basic&fmt=json&api_key=#{ENV["EDMUNDS_API_KEY"]}")
+		request = HTTParty.get(url)
 
 	end
 
