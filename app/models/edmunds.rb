63e602b1499e1242d5e76e1fe72de0206c1e7186
albertbahia <albert.bahia1@gmail.com>
2015-01-01 22:15:42 -0500

    successful api request with api key

--- app/models/edmunds.rb
+++ app/models/edmunds.rb
@@ -1,7 +1,7 @@
 class Edmunds
 
 	def self.car_search(query)
-		url = URI.escape("https://api.edmunds.com/api/vehicle/v2/makes?state=used&year=2010&view=basic&fmt=json&api_key=#{ENV["EDMUNDS_API_KEY"]}")
+		url = URI.escape("https://api.edmunds.com/api/vehicle/v2/makes?state=used&year=#{query}&view=basic&fmt=json&api_key=#{ENV["EDMUNDS_API_KEY"]}")
 		request = HTTParty.get(url)
 
 	end
